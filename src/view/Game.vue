22e70d62ef41250d1c7491c31b0fd883b0de882d
xhemj <xhemj2680@163.com>
2023-06-17 16:53:55 +0800

    优化获胜判断的条件

--- src/view/Game.vue
+++ src/view/Game.vue
@@ -6,70 +6,112 @@
           isGameStart ? "游戏开始，长按投票出局" : "一人选择一张牌，不许偷看哦~"
         }}
       </h3>
-      <router-link :to="{ name: 'setting' }" class="game-btn btn-small w-fit text-base">
+      <router-link
+        :to="{ name: 'setting' }"
+        class="game-btn btn-small w-fit text-base"
+      >
         重新开始
       </router-link>
     </div>
     <!-- 游戏区 -->
     <div v-if="gameWords.length" class="flex flex-row flex-wrap w-full my-6">
-      <div v-for="(item, index) in gameWords" :key="`${index}-${item.word}`"
-        class="game-card w-1/2 p-3 cursor-pointer relative" :class="{
-              'pointer-events-none game-eliminated': item.isEliminated,
-            }" @click="() => {
-          if (!item.isViewed) {
-            showCardModal(item, index);
+      <div
+        v-for="(item, index) in gameWords"
+        :key="`${index}-${item.word}`"
+        class="game-card w-1/2 p-3 cursor-pointer relative"
+        :class="{
+          'pointer-events-none game-eliminated': item.isEliminated,
+        }"
+        @click="
+          () => {
+            if (!item.isViewed) {
+              showCardModal(item, index);
+            }
           }
-        }
-        " v-on-long-press="[
+        "
+        v-on-long-press="[
           () => {
             if (isGameStart) {
               eliminate(item, index);
             }
           },
           { delay: 500 },
-        ]">
-        <div class="absolute top-3 bottom-3 left-3 right-3 w-auto h-auto flex justify-center items-center">
+        ]"
+      >
+        <div
+          class="absolute top-3 bottom-3 left-3 right-3 w-auto h-auto flex justify-center items-center"
+        >
           <!-- 未查看 -->
-          <div v-if="!item.isViewed" class="absolute text-4xl font-bold text-white w-full text-center">
-            <div class="stroke-text stroke select-none font-sans w-[1em] mx-auto">
+          <div
+            v-if="!item.isViewed"
+            class="absolute text-4xl font-bold text-white w-full text-center"
+          >
+            <div
+              class="stroke-text stroke select-none font-sans w-[1em] mx-auto"
+            >
               {{ index + 1 }} 号
-            </div>
-            <!-- 调试专用 -->
-            <div v-if="game.isDebugMode" class="text-lg mt-2">
-              {{ item.isSpy ? (item.isBlank ? "白板" : "卧底") : "平民" }}
             </div>
           </div>
           <!-- 已查看 -->
           <div v-else class="absolute text-black w-full text-center">
             <!-- 卡片任务头像 -->
             <!-- <img :key="`${index}-${item.word}`" class="words-face !h-20 !w-20" :src="getRandomFace(index)" /> -->
-            <div :key="`${index}-${item.word}`" class="words-face !h-20 !w-20 bg-cover bg-center" :style="{
+            <div
+              :key="`${index}-${item.word}`"
+              class="words-face !h-20 !w-20 bg-cover bg-center"
+              :style="{
                 backgroundImage: `url(${getRandomFace(index)})`,
-              }" />
+              }"
+            />
             <div class="text-lg mt-0 text-center">
               {{ index + 1 + "号" }}
             </div>
-            <div class="game-btn btn-small btn-review mt-1 w-fit mx-auto text-base text-white" :class="{
-              block: isGameStart,
-              '!hidden': !isGameStart,
-              disabled: (item.isViewed && item.isReviewed) || item.isEliminated,
-            }" @click="showCardModal(item, index)">
+            <div
+              class="game-btn btn-small btn-review mt-1 w-fit mx-auto text-base text-white"
+              :class="{
+                block: isGameStart,
+                '!hidden': !isGameStart,
+                disabled:
+                  (item.isViewed && item.isReviewed) || item.isEliminated,
+              }"
+              @click="showCardModal(item, index)"
+            >
               忘词
             </div>
           </div>
+          <!-- 调试专用 -->
+          <div
+            v-if="game.isDebugMode"
+            class="stroke debug-text text-lg absolute bottom-6"
+          >
+            {{ item.isSpy ? (item.isBlank ? "白板" : "卧底") : "平民" }}
+          </div>
         </div>
-        <img class="w-full h-auto" :src="item.isViewed
-            ? 'https://staticoss.xhemj.work/spy.xhemj.com/card-viewed.png'
-            : 'https://staticoss.xhemj.work/spy.xhemj.com/card.png'
-          " />
+        <img
+          class="w-full h-auto"
+          :src="
+            item.isViewed
+              ? 'https://staticoss.xhemj.work/spy.xhemj.com/card-viewed.png'
+              : 'https://staticoss.xhemj.work/spy.xhemj.com/card.png'
+          "
+        />
       </div>
     </div>
     <!-- 弹出框 -->
-    <input ref="gameModalRef" type="checkbox" :id="gameModalId" class="modal-toggle" />
+    <input
+      ref="gameModalRef"
+      type="checkbox"
+      :id="gameModalId"
+      class="modal-toggle"
+    />
     <div class="modal flex flex-col justify-center items-center">
       <div class="words-modal modal-box relative">
         <div class="w-full mt-2">
-          <img :key="`${currentViewId}-${currentViewText}`" class="words-face" :src="getRandomFace(currentViewId)" />
+          <img
+            :key="`${currentViewId}-${currentViewText}`"
+            class="words-face"
+            :src="getRandomFace(currentViewId)"
+          />
           <div class="text-base mt-0 mb-5 text-center">
             {{ currentViewId + 1 + "号" }}
           </div>
@@ -82,13 +124,17 @@
         </p>
       </div>
       <div class="w-full mt-8 text-center">
-        <div class="game-btn mx-auto" @click="() => {
-          if (gameModalRef) {
-            onAfterCloseModal();
-            gameModalRef.checked = false;
-          }
-        }
-          ">
+        <div
+          class="game-btn mx-auto"
+          @click="
+            () => {
+              if (gameModalRef) {
+                onAfterCloseModal();
+                gameModalRef.checked = false;
+              }
+            }
+          "
+        >
           我记下了
         </div>
       </div>
@@ -120,7 +166,7 @@ const unEliminatedCivilianIndexList: Ref<number[]> = ref([]);
 const currentViewId = ref(0);
 const currentViewText = ref("");
 
-let onAfterCloseModal: any = () => { };
+let onAfterCloseModal: any = () => {};
 
 const isGameStart = computed(() => {
   return gameWords.value.length > 0 && unViewedWords.value.length === 0;
@@ -149,24 +195,44 @@ const showCardModal = (item: GameWord, index) => {
 // 淘汰
 const eliminate = (item: GameWord, index: number) => {
   if (item.isSpy) {
+    // 卧底被淘汰
     unEliminatedSpyIndexList.value = unEliminatedSpyIndexList.value.filter(
       (i) => i !== index
     );
   } else {
+    // 平民被淘汰
     unEliminatedCivilianIndexList.value =
       unEliminatedCivilianIndexList.value.filter((i) => i !== index);
   }
+
   item.isEliminated = true;
-  if (
-    unEliminatedCivilianIndexList.value.length === 0 ||
-    unEliminatedSpyIndexList.value.length === 0
-  ) {
-    if (unEliminatedSpyIndexList.value.length === 0) {
+
+  // 判断游戏是否结束
+  // 1. 平民全被淘汰
+  const isNoUnEliminatedCivilian =
+    unEliminatedCivilianIndexList.value.length === 0;
+  // 2. 卧底全被淘汰
+  const isNoUnEliminatedSpy = unEliminatedSpyIndexList.value.length === 0;
+  // 3. 只剩下一个平民且剩余一个卧底
+  const isOnlyCivilianAndSpy =
+    unEliminatedCivilianIndexList.value.length === 1 &&
+    unEliminatedSpyIndexList.value.length >= 1;
+
+  // 卧底获胜条件：平民全被淘汰 || 只剩下一个平民且剩余一个卧底
+  const isSpyWin = isNoUnEliminatedCivilian || isOnlyCivilianAndSpy;
+  // 平民获胜条件：卧底全被淘汰
+  const isCivilianWin = isNoUnEliminatedSpy;
+
+  const isGameOver = isSpyWin || isCivilianWin;
+  if (isGameOver) {
+    if (isCivilianWin) {
       // 平民胜利
       game.result.win = "civilian";
-    } else if (unEliminatedCivilianIndexList.value.length === 0) {
+    } else if (isSpyWin) {
       // 卧底胜利
       game.result.win = "spy";
+    } else {
+      return alert("游戏异常，请重新开始");
     }
     game.result.gameWords = gameWords.value.map((item, index) => {
       return {
@@ -246,6 +312,11 @@ onMounted(() => {
 .stroke-text {
   --font-color: rgb(238, 223, 255);
   --stroke-color: rgb(102, 57, 220);
+}
+
+.debug-text {
+  --font-color: #fff;
+  --stroke-color: #333;
 }
 
 .modal {
